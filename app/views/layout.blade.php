74a9c525920a7d6b572422450e445ed0a18be919
Matthew Davies <daviesgeek@icloud.com>
2014-03-23 17:35:58 -0700

    Creating new files for stuff

---
+++ app/views/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/main.css">
+    <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ app/views/login.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+  <h1>Login here!</h1>
+@stop
