57b81a48cddd46529d0e188e216285ce3ea4ba2e
Matthew Davies <daviesgeek@icloud.com>
2014-04-05 16:19:03 -0700

    Added makeURL() JS function for making URLs

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -11,5 +11,10 @@
     @yield('content')
   </body>
   {{ HTML::script('assets/public.js') }}
+  <script type="text/javascript">
+    window.makeURL = function(url) {
+      return '{{ URL::to('/') }}'+'/'+url.replace(/^\/|\/$/g, '');
+    }
+  </script>
   <script type="text/javascript">require('assets/js/public')</script>
 </html>
