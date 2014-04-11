0f5e4f9ecdf442151668d744a241c20418387a73
Matthew Davies <daviesgeek@icloud.com>
2014-04-10 23:29:52 -0700

    Added admin template to Laravel for Angular

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -1,10 +1,12 @@
-<html>
-  <head>
-    {{ HTML::style('_/css/app.css') }}
-    {{ HTML::script('_/js/app.js') }}
-  </head>
-  <title>Admin</title>
-  <body>
-    <h1>Admin</h1>
-  </body>
+<!DOCTYPE html>
+<html lang="en-US" ng-app="app">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+    {{ HTML::style('assets/_/css/app.css') }}
+    {{ HTML::script('assets/_/js/vendor.js') }}
+    {{ HTML::script('assets/_/js/views.js') }}
+    {{ HTML::script('assets/_/js/app.js') }}
+  <script type="text/javascript">require('app')</script>
+  @include('admin-angular')
 </html>
