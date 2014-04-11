c7277f3ba31bb9710a5841e445b8f0aabebd17ac
Matthew Davies <daviesgeek@icloud.com>
2014-04-10 21:17:09 -0700

    Main admin controller returns view

--- app/controllers/Admin/AdminController.php
+++ app/controllers/Admin/AdminController.php
@@ -1,11 +1,12 @@
 <?php namespace Admin;
-
 /**
  * Main Admin controller
+ * @author Matthew Davies <daviesgeek@icloud.com>
+ * @created April 10th, 2014
  */
 
 class AdminController extends BaseController {
   public function index() {
-    
+    return \View::make('admin');    
   }
 }
