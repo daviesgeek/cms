e4615ae66222b3792a0105b1281439141b77516e
Matthew Davies <daviesgeek@icloud.com>
2014-04-19 22:45:22 -0700

    Added 403 errors to the English lang file

--- app/lang/en/errors.php
+++ app/lang/en/errors.php
@@ -3,6 +3,8 @@
 return array(
 
   '404' => 'Sorry, the page you are looking for does not exist',
-  '403' => 'Sorry, you are not authorized to access this page'
+  '403' => 'Sorry, you are not authorized to access this page',
 
+  '403json' => 'You do not have access to view this page',
+  '403login' => 'Login is required',
 );
