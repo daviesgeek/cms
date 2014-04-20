74b56b3d72c19878709d96c84647c86496a355b3
Matthew Davies <daviesgeek@icloud.com>
2014-04-20 00:32:24 -0700

    Added vendor.css to head
    Added font awesome
    Added classes for admin theme to the body

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html lang="en-US" ng-app="admin/app">
+<html lang="en-US" ng-app="admin/app" class="skin-black wysihtml5-supported  pace-done">
 <head>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+    {{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css') }}
+    {{ HTML::style('assets/_admin/css/vendor.css') }}
     {{ HTML::style('assets/_admin/css/app.css') }}
     {{ HTML::script('assets/_admin/js/vendor.js') }}
     {{ HTML::script('assets/_admin/js/views.js') }}
