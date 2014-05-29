0131f5b5db5323c583bf45381d2dae875a1b52bb
Matthew Davies <daviesgeek@icloud.com>
2014-05-28 22:59:53 -0700

    Moved authentication out of the API base controller

---
+++ app/controllers/Api/ApiBaseController.php
@@ -0,0 +1,20 @@
+<?php namespace Api;
+/**
+ * Base class for the API controllers
+ * @author Matthew Davies <daviesgeek@icloud.com>
+ * @created March 30th, 2014
+ */
+
+use App;
+use Lang;
+use Route;
+use Sentry;
+
+class ApiBaseController extends \BaseController {
+
+  public function __construct() {
+
+
+  }
+
+}
